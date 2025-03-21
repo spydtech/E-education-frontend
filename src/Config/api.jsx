5fa8d0b4f4b370a7038d9e4e371b67a6953d1ff7
spydtech <166598988+spydtech@users.noreply.github.com>
2025-03-21 12:15:25 +0530

    Update api.jsx

--- src/Config/api.jsx
+++ src/Config/api.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // export const API_BASE_URL = "https://3.109.30.200:8082";
 // Use HTTP instead of HTTPS
-export const API_BASE_URL = "https://13.126.181.47:8082";
+export const API_BASE_URL = "http://3.6.36.172:8082";
 
 
 
