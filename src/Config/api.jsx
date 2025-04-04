b979b99b2e809de63cae0a02604b1ebfb6c185b3
Prabhashyam <116556508+7893787849@users.noreply.github.com>
2025-04-05 00:00:28 +0530

    update  code

--- src/Config/api.jsx
+++ src/Config/api.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 
-export const API_BASE_URL = "https://3.109.30.200:8082"; // Use HTTP instead of HTTPS
+export const API_BASE_URL = "https://api.e-education.in"; // Use HTTP instead of HTTPS
 
 
 
